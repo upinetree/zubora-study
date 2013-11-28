c1ffda01b45ade49f1355c38d2ea9471bf4c84d2
upinetree <t.matsumoto@everyleaf.com>
2013-11-28 16:04:47 +0900

    refs #1 Rename table name 'items' to 'expenses'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Zubora::Application.routes.draw do
-  resources :items
+  resources :expenses
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
