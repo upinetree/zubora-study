d28e105b8e47c1002d49189a115def716b27fa6e
upinetree <t.matsumoto@everyleaf.com>
2013-11-28 16:28:40 +0900

    refs #1 rename culumn name price to amount.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -59,6 +59,6 @@ class ExpensesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def expense_params
-      params.require(:expense).permit(:price, :category_id, :memo)
+      params.require(:expense).permit(:amount, :category_id, :memo)
     end
 end
