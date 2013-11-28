e8101abb28be2f1de929021f796a2cded8dec1ae
upinetree <t.matsumoto@everyleaf.com>
2013-11-28 10:59:57 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zubora::Application.config.secret_key_base = '6085369f62e0b76e2054bdb95c5b7bbf6b4aec4cf475f7e1cc1d5a53f85b076542b96780f48bc0416bde6403f0f07d1253b40b8d0b3f6f69533db0eec69a56ea'
