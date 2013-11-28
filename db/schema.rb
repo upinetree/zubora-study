7d5c9de5fb3923c0d18c61d02c7017ee96be2db5
upinetree <essequake@gmail.com>
2013-11-29 00:15:15 +0900

    refs #1 Change colmun of expenses amount not to be null

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131128074131) do
+ActiveRecord::Schema.define(version: 20131128144354) do
 
   create_table "expenses", force: true do |t|
-    t.integer  "amount"
+    t.integer  "amount",      null: false
     t.integer  "category_id"
     t.string   "memo"
     t.datetime "created_at"
