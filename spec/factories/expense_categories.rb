e81d7d76e04ff93dde9acb59ce90dbdd59a12287
upinetree <t.matsumoto@everyleaf.com>
2013-12-30 16:10:03 +0900

    refs #10 Use random selected color for factory of expense_category

--- spec/factories/expense_categories.rb
+++ spec/factories/expense_categories.rb
@@ -3,6 +3,10 @@
 FactoryGirl.define do
   factory :expense_category do
     sequence(:name){|n| "Category#{n}"}
-    color "#FF6347"
+    color { generate(:sample_color) }
+  end
+
+  sequence :sample_color do
+    %w(#FF6347 #3cb371 #4169e1 #2f4f4f #ffd700).shuffle.first
   end
 end
